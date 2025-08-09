ffd587d1884c90289c078659fdc35ec41359f3d7
Andy Park <atpark333@gmail.com>
2025-08-09 17:11:03 +0900

    Render image asset to screen

--- DungeonSlime/Game1.cs
+++ DungeonSlime/Game1.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Xna.Framework;
+﻿using System;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using MonoGameLibrary;
@@ -7,6 +8,8 @@ namespace DungeonSlime;
 
 public class Game1 : Core
 {
+    private Texture2D _logo;
+    
     public Game1(): base("Dungeon Slime", 1280, 720, false)
     {
         
@@ -20,7 +23,7 @@ public class Game1 : Core
 
     protected override void LoadContent()
     {
-        // TODO: use this.Content to load your game content here
+        _logo = Content.Load<Texture2D>("images/logo");
     }
 
     protected override void Update(GameTime gameTime)
@@ -36,9 +39,28 @@ public class Game1 : Core
 
     protected override void Draw(GameTime gameTime)
     {
-        GraphicsDevice.Clear(Color.MonoGameOrange);
+        GraphicsDevice.Clear(Color.CornflowerBlue);
 
-        // TODO: Add your drawing code here
+        // Begin sprite batch to prepare for rendering
+        SpriteBatch.Begin();
+        
+        // Draw the logo texture
+        SpriteBatch.Draw(
+            _logo,              // texture
+            new Vector2(        // position
+                (Window.ClientBounds.Width * 0.5f) - (_logo.Width * 0.25f),
+                (Window.ClientBounds.Height * 0.5f) - (_logo.Height * 0.25f)),
+            null,               // source rect
+            Color.White,        // color mask tint
+            0.0f,               // rotation
+            Vector2.Zero,       // origin
+            0.5f,               // scale
+            SpriteEffects.None, // effects
+            0.0f                // layer depth
+        );
+        
+        // ALWAYS end the sprite batch when finished
+        SpriteBatch.End();
 
         base.Draw(gameTime);
     }
