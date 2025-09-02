2ba17de7cd8fd054246688407184ba0978d0cb02
Andy Park <atpark333@gmail.com>
2025-09-02 20:38:14 +0900

    Add InputManager to game core

--- MonoGameLibrary/Core.cs
+++ MonoGameLibrary/Core.cs
@@ -2,69 +2,121 @@ using System;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using MonoGameLibrary.Input;
 
 namespace MonoGameLibrary;
 
 public class Core : Game
 {
-    // Singleton
-    private static Core _sInstance;
+    internal static Core s_instance;
 
-    public static Core Instance => _sInstance;
-    
+    /// <summary>
+    /// Gets a reference to the Core instance.
+    /// </summary>
+    public static Core Instance => s_instance;
+
+    /// <summary>
+    /// Gets the graphics device manager to control the presentation of graphics.
+    /// </summary>
     public static GraphicsDeviceManager Graphics { get; private set; }
-    
+
+    /// <summary>
+    /// Gets the graphics device used to create graphical resources and perform primitive rendering.
+    /// </summary>
     public static new GraphicsDevice GraphicsDevice { get; private set; }
-    
+
+    /// <summary>
+    /// Gets the sprite batch used for all 2D rendering.
+    /// </summary>
     public static SpriteBatch SpriteBatch { get; private set; }
-    
+
+    /// <summary>
+    /// Gets the content manager used to load global assets.
+    /// </summary>
     public static new ContentManager Content { get; private set; }
 
+    /// <summary>
+    /// Gets a reference to the input management system.
+    /// </summary>
+    public static InputManager Input { get; private set; }
+
+    /// <summary>
+    /// Gets or Sets a value that indicates if the game should exit when the esc key on the keyboard is pressed.
+    /// </summary>
+    public static bool ExitOnEscape { get; set; }
+
+    /// <summary>
+    /// Creates a new Core instance.
+    /// </summary>
+    /// <param name="title">The title to display in the title bar of the game window.</param>
+    /// <param name="width">The initial width, in pixels, of the game window.</param>
+    /// <param name="height">The initial height, in pixels, of the game window.</param>
+    /// <param name="fullScreen">Indicates if the game should start in fullscreen mode.</param>
     public Core(string title, int width, int height, bool fullScreen)
     {
         // Ensure that multiple cores are not created.
-        if (_sInstance != null)
+        if (s_instance != null)
         {
-            throw new InvalidOperationException($"Only one instance of the {nameof(Core)} is allowed.");
+            throw new InvalidOperationException($"Only a single Core instance can be created");
         }
-        
+
         // Store reference to engine for global member access.
-        _sInstance = this;
-        
+        s_instance = this;
+
         // Create a new graphics device manager.
         Graphics = new GraphicsDeviceManager(this);
-        
+
         // Set the graphics defaults.
         Graphics.PreferredBackBufferWidth = width;
         Graphics.PreferredBackBufferHeight = height;
         Graphics.IsFullScreen = fullScreen;
-        
+
         // Apply the graphic presentation changes.
         Graphics.ApplyChanges();
-        
+
         // Set the window title.
         Window.Title = title;
-        
+
         // Set the core's content manager to a reference of the base Game's
-        // content manager
+        // content manager.
         Content = base.Content;
-        
+
         // Set the root directory for content.
         Content.RootDirectory = "Content";
-        
-        // Mouse is visible by default
+
+        // Mouse is visible by default.
         IsMouseVisible = true;
+
+        // Exit on escape is true by default
+        ExitOnEscape = true;
     }
 
     protected override void Initialize()
     {
         base.Initialize();
-        
+
         // Set the core's graphics device to a reference of the base Game's
         // graphics device.
         GraphicsDevice = base.GraphicsDevice;
-        
+
         // Create the sprite batch instance.
         SpriteBatch = new SpriteBatch(GraphicsDevice);
+
+        // Create a new input manager.
+        Input = new InputManager();
+    }
+
+    protected override void Update(GameTime gameTime)
+    {
+        // Update the input manager.
+        Input.Update(gameTime);
+
+        if (ExitOnEscape && Input.Keyboard.IsKeyDown(Keys.Escape))
+        {
+            Exit();
+        }
+
+        base.Update(gameTime);
     }
 }
